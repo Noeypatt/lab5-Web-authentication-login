0f60dc8839b74a04e316d7c3a9c02e82e02135a0
Noeypatt <43218541+Noeypatt@users.noreply.github.com>
2019-02-28 22:40:01 +0700

    V2

--- index.js
+++ index.js
@@ -2,28 +2,47 @@ var express = require('express'),
   app = express(),
   bodyParser = require('body-parser');
 
-app.set('views', './views')
-app.set('view engine', 'ejs')
+  app.set('views', './views')
+  app.set('view engine', 'ejs')
 
+var session = require('express-session')
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 },  //session
+   resave : false, saveUninitialized: false }))
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });    //body-parser
 app.use(express.static(__dirname + '/public'));
 
-app.post('/login', urlencodedParser, function (req, res) {
+app.use(function(req, res, next) {
+  var sess = req.session
+  next();
+})
 
-  if (req.body.password == '1224' && req.body.email.value !== "") {
-    var email = req.body.email;
+app.post('/login', urlencodedParser, function (req, res) {
+  console.log("Login");
+  console.log("User: "+req.body.email);
+
+  req.session.email =  req.body.email;
+  req.session.password =  req.body.password;
+
+  if (req.session.email == "5935512027" && req.session.password == '1224') {
+    var email = req.session.email;
+
     app.get('/admin', function (req, res) {
       res.render('admin',{ hello: "Hello",login: email,link:'logout'})
+      console.log("Complete");
     });
   }
 
   else {
-    app.get('/admin', function (req, res) {
+    app.get('/admin', function (req, res) {                                           //ejs
       res.render('admin', { hello: "",login: 'Please login first.',link:'login' })
+      console.log("Fail");
     })
 
   }
-
 });
+
+
+
 app.listen(8000);
